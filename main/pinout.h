c86dcc6cf96e61bc91cf821f4cf522a80709d510
kraiha1610 <lekhai@089gmail.com>
2026-06-17 10:45:35 +0700

    code do an

---
+++ main/pinout.h
@@ -0,0 +1,20 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+// I2C cho LCD + SHT3x
+#define SDA_PIN     21
+#define SCL_PIN     22
+
+// Nút nhấn
+#define BTN_SET     13
+#define BTN_UP      14
+#define BTN_DOWN    27
+#define BTN_MODE    26
+
+// Relay & SSR
+#define RELAY_TURN  18   // đảo trứng
+#define RELAY_FAN   5   // quạt hút
+#define RELAY_HUMI  4  // tạo ẩm
+#define RELAY_HEATER     23  // dự phòng
+
+#endif
